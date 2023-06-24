f3a3548b055234902140de065b22e2d390db13e9
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-06-24 02:54:27 -0300

    Se creo los modelos TipoHabitacion y Habitacion.

--- project/apps/hotel/admin.py
+++ project/apps/hotel/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.site_title = "El Jardin Secreto"
+admin.site.site_header = "El Jardin Secreto Admin"
+
+@admin.register(models.TipoHabitacion)
+class TipoHabitacionAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion")
+    search_fields = ("nombre",)
+    list_filter = ("nombre",)
+    ordering = ("nombre",)
+
+
+@admin.register(models.Habitacion)
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display = (
+        "tipo",
+        "numero",
+        "precio_x_dia",
+        "disponible",
+        "imagen",
+    )
+    
+    search_fields = ("tipo__nombre","numero",)
+    list_filter = ("tipo__nombre",)
+    ordering = (
+        "tipo",
+        "numero",
+    )
--- project/apps/hotel/models.py
+++ project/apps/hotel/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class TipoHabitacion(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Tipo de habitacion"
+        verbose_name_plural = "Tipos de habitaciones"
+
+    def __str__(self):
+        return self.nombre
+    
+class Habitacion(models.Model):
+    tipo = models.ForeignKey(TipoHabitacion, on_delete=models.CASCADE)
+    numero = models.PositiveIntegerField(unique=True)
+    precio_x_dia = models.DecimalField(max_digits=10, decimal_places=2,)
+    disponible = models.BooleanField(default=False)
+    imagen = models.ImageField(upload_to="img/", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Habitacion"
+        verbose_name_plural = "Habitaciones"
+
+    def __str__(self):
+        return f"Habitacion N° {self.numero}"
