d196398b3edc182d16eadc3fab960211cf900bb1
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-06-25 00:18:41 -0300

    Se modifico permisos a views.

--- project/apps/hotel/urls.py
+++ project/apps/hotel/urls.py
@@ -6,17 +6,17 @@ from django.contrib.admin.views.decorators import staff_member_required
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     # Tipo de habitacion
-    path("tipohabitacion/detail/<int:pk>", staff_member_required(views.TipoHabitacionDetail.as_view()), name="tipohabitacion_detail"),
-    path("tipohabitacion/list/", staff_member_required(views.TipoHabitacionList.as_view()), name="tipohabitacion_list"),
-    path("tipohabitacion/create/", staff_member_required(views.TipoHabitacionCreateView.as_view()), name="tipohabitacion_create"),
-    path("tipohabitacion/delete/<int:pk>", staff_member_required(views.TipoHabitacionDelete.as_view()), name="tipohabitacion_delete"),
-    path("tipohabitacion/update/<int:pk>", staff_member_required(views.TipoHabitacionUpdate.as_view()), name="tipohabitacion_update"),
+    path("tipohabitacion/detail/<int:pk>", views.TipoHabitacionDetail.as_view(), name="tipohabitacion_detail"),
+    path("tipohabitacion/list/", views.TipoHabitacionList.as_view(), name="tipohabitacion_list"),
+    path("tipohabitacion/create/", views.TipoHabitacionCreateView.as_view(), name="tipohabitacion_create"),
+    path("tipohabitacion/delete/<int:pk>", views.TipoHabitacionDelete.as_view(), name="tipohabitacion_delete"),
+    path("tipohabitacion/update/<int:pk>", views.TipoHabitacionUpdate.as_view(), name="tipohabitacion_update"),
     # Habitacion
     path("habitacion/detail/<int:pk>", views.HabitacionDetail.as_view(), name="habitacion_detail"),
     path("habitacion/list/", views.HabitacionList.as_view(), name="habitacion_list"),
-    path("habitacion/create/", staff_member_required(views.HabitacionCreateView.as_view()), name="habitacion_create"),
-    path("habitacion/delete/<int:pk>", staff_member_required(views.HabitacionDelete.as_view()), name="habitacion_delete"),
-    path("habitacion/update/<int:pk>", staff_member_required(views.HabitacionUpdate.as_view()), name="habitacion_update"),
+    path("habitacion/create/", views.HabitacionCreateView.as_view(), name="habitacion_create"),
+    path("habitacion/delete/<int:pk>", views.HabitacionDelete.as_view(), name="habitacion_delete"),
+    path("habitacion/update/<int:pk>", views.HabitacionUpdate.as_view(), name="habitacion_update"),
     # Reserva
     path("reserva/detail/<int:pk>", views.ReservaDetail.as_view(), name="reserva_detail"),
     path("reserva/list/", views.ReservaList.as_view(), name="reserva_list"),
--- project/apps/hotel/views.py
+++ project/apps/hotel/views.py
@@ -17,10 +17,14 @@ class IndexView(TemplateView):
     
 # Tipo de Habitacion
 
-class TipoHabitacionDetail(LoginRequiredMixin, DetailView):
+class TipoHabitacionDetail(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = models.TipoHabitacion
+    def test_func(self):
+        """ Devuelve True si el usuario es del staff."""
+        return self.request.user.is_staff
+    
 
-class TipoHabitacionList(LoginRequiredMixin, ListView):
+class TipoHabitacionList(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = models.TipoHabitacion
     def get_queryset(self):
         if self.request.GET.get("consulta"):
@@ -29,8 +33,11 @@ class TipoHabitacionList(LoginRequiredMixin, ListView):
         else:
             object_list = models.TipoHabitacion.objects.all()
         return object_list
+    def test_func(self):
+        """ Devuelve True si el usuario es del staff."""
+        return self.request.user.is_staff
 
-class TipoHabitacionCreateView(LoginRequiredMixin, CreateView):
+class TipoHabitacionCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = models.TipoHabitacion
     form_class = forms.TipoHabitacionForm
     success_url = reverse_lazy("hotel:tipohabitacion_list")
@@ -38,8 +45,11 @@ class TipoHabitacionCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         messages.success(self.request, "Tipo de habitacion creada correctamente.", extra_tags="alert alert-success")
         return super().form_valid(form)
+    def test_func(self):
+        """ Devuelve True si el usuario tiene permiso."""
+        return self.request.user.has_perm('hotel.add_tipohabitacion')
 
-class TipoHabitacionDelete(LoginRequiredMixin, DeleteView):
+class TipoHabitacionDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = models.TipoHabitacion
     template_name = "hotel/confirm_delete.html"
     success_url = reverse_lazy("hotel:tipohabitacion_list")
@@ -47,8 +57,12 @@ class TipoHabitacionDelete(LoginRequiredMixin, DeleteView):
     def get_success_url(self):
             messages.success(self.request, "Tipo de habitacion eliminada correctamente.", extra_tags="alert alert-danger")
             return super().get_success_url()
+    
+    def test_func(self):
+        """ Devuelve True si el usuario tiene permiso."""
+        return self.request.user.has_perm('hotel.delete_tipohabitacion')
 
-class TipoHabitacionUpdate(LoginRequiredMixin, UpdateView):
+class TipoHabitacionUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = models.TipoHabitacion
     success_url = reverse_lazy("hotel:tipohabitacion_list")
     form_class = forms.TipoHabitacionForm
@@ -56,7 +70,9 @@ class TipoHabitacionUpdate(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         messages.success(self.request, "Tipo de habitacion actualizada correctamente.", extra_tags="alert alert-success")
         return super().form_valid(form)
-
+    def test_func(self):
+        """ Devuelve True si el usuario tiene permiso."""
+        return self.request.user.has_perm('hotel.change_tipohabitacion')
 
 # Habitacion
 
@@ -73,7 +89,7 @@ class HabitacionList(LoginRequiredMixin, ListView):
             object_list = models.Habitacion.objects.all()
         return object_list
 
-class HabitacionCreateView(LoginRequiredMixin, CreateView):
+class HabitacionCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = models.Habitacion
     form_class = forms.HabitacionForm
     success_url = reverse_lazy("hotel:habitacion_list")
@@ -81,8 +97,12 @@ class HabitacionCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         messages.success(self.request, "Habitacion creada correctamente.", extra_tags="alert alert-success")
         return super().form_valid(form)
+    
+    def test_func(self):
+        """ Devuelve True si el usuario tiene permiso."""
+        return self.request.user.has_perm('hotel.add_habitacion')
 
-class HabitacionDelete(LoginRequiredMixin, DeleteView):
+class HabitacionDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = models.Habitacion
     template_name = "hotel/confirm_delete.html"
     success_url = reverse_lazy("hotel:habitacion_list")
@@ -90,8 +110,12 @@ class HabitacionDelete(LoginRequiredMixin, DeleteView):
     def get_success_url(self):
             messages.success(self.request, "Habitacion eliminada correctamente.", extra_tags="alert alert-danger")
             return super().get_success_url()
+    def test_func(self):
+        """ Devuelve True si el usuario tiene permiso."""
+        return self.request.user.has_perm('hotel.delete_habitacion')
 
-class HabitacionUpdate(LoginRequiredMixin, UpdateView):
+
+class HabitacionUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = models.Habitacion
     success_url = reverse_lazy("hotel:habitacion_list")
     form_class = forms.HabitacionForm
@@ -99,6 +123,10 @@ class HabitacionUpdate(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         messages.success(self.request, "Habitacion actualizada correctamente.", extra_tags="alert alert-success")
         return super().form_valid(form)
+
+    def test_func(self):
+        """ Devuelve True si el usuario tiene permiso."""
+        return self.request.user.has_perm('hotel.change_habitacion')
 
 
 # Reserva
@@ -140,27 +168,27 @@ class ReservaDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = models.Reserva
     template_name = "hotel/confirm_delete.html"
     success_url = reverse_lazy("hotel:reserva_list")
-    def test_func(self):
-        """Determina el acceso a la vista, devuelve True si el usuario es el cliente o si es personal del staff, 
-        False de lo contrario."""
-        reserva = self.get_object()
-        return reserva.cliente == self.request.user or self.request.user.is_staff
-    
+
     def get_success_url(self):
             messages.success(self.request, "Reserva eliminada correctamente.", extra_tags="alert alert-danger")
             return super().get_success_url()
-
+    
+    def test_func(self):
+        """Determina el acceso a la vista, devuelve True si el usuario autenticado es el cliente o si tiene permiso para eliminar la reserva."""
+        reserva = self.get_object()
+        return reserva.cliente == self.request.user or self.request.user.has_perm('hotel.delete_reserva')
+    
 class ReservaUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = models.Reserva
     success_url = reverse_lazy("hotel:reserva_list")
-
     form_class = forms.ReservaForm
-    def test_func(self):
-        """Determina el acceso a la vista, devuelve True si el usuario es el cliente o si es personal del staff, False de lo contrario."""
-        reserva = self.get_object()
-        return reserva.cliente == self.request.user or self.request.user.is_staff
-    
+
     def form_valid(self, form):
         messages.success(self.request, "Reserva actualizada correctamente.", extra_tags="alert alert-success")
         return super().form_valid(form)
-
+    
+    def test_func(self):
+        """Determina el acceso a la vista, devuelve True si el usuario autenticado es el cliente o si tiene permiso para modificar la reserva."""
+        reserva = self.get_object()
+        return reserva.cliente == self.request.user or self.request.user.has_perm('hotel.change_reserva')
+    
