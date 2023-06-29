8cdcc635459e4df00e54eb720d254b9fc723d4ea
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-06-29 18:46:34 -0300

    Vistas de blog modificadas

--- project/apps/blog/views.py
+++ project/apps/blog/views.py
@@ -1,10 +1,10 @@
-
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic import DetailView, ListView
 from django.urls import reverse_lazy
 from .forms import ReseñaForm
 from .models import Reseña
+from django.contrib import messages
 
 # Create your views here.
 class ReseñaDetail(LoginRequiredMixin, DetailView):
@@ -12,14 +12,23 @@ class ReseñaDetail(LoginRequiredMixin, DetailView):
 
 class ReseñaList(LoginRequiredMixin, ListView):
     model = Reseña
-
+    def get_queryset(self):
+        """Filtra todas las reseñas cuyo autor contenga el texto ingresado."""
+        if self.request.GET.get("consulta"):
+            query = self.request.GET.get("consulta")
+            object_list = Reseña.objects.filter(autor__username__icontains=query)
+        else:
+            object_list = Reseña.objects.all()
+        return object_list
 
 class ReseñaCreateView(LoginRequiredMixin, CreateView):
     model = Reseña
     form_class = ReseñaForm
     success_url = reverse_lazy("blog:reseña_list")
     def form_valid(self, form):
+        """Autor de la reseña = usuario logueado."""
         form.instance.autor = self.request.user
+        messages.success(self.request, "Reseña creada correctamente.", extra_tags="alert alert-success")
         return super().form_valid(form)
     
 class ReseñaUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -27,8 +36,24 @@ class ReseñaUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     success_url = reverse_lazy("blog:reseña_list")
     form_class = ReseñaForm
 
+    def test_func(self):
+        """Determina el acceso a la vista, devuelve True si el usuario es el autor de la reseña."""
+        reseña = self.get_object()
+        return reseña.autor == self.request.user
+    
+    def form_valid(self, form):
+        messages.success(self.request, "Reseña actualizada correctamente.", extra_tags="alert alert-success")
+        return super().form_valid(form)
     
 class ReseñaDelete(LoginRequiredMixin, DeleteView):
     model = Reseña
     success_url = reverse_lazy("blog:reseña_list")
 
+    def test_func(self):
+        """Determina el acceso a la vista, devuelve True si el usuario es el autor de la reseña o si es miembro del staff."""
+        reseña = self.get_object()
+        return reseña.autor == self.request.user or self.request.user.is_staff
+    
+    def get_success_url(self):
+            messages.success(self.request, "Reseña eliminada correctamente.", extra_tags="alert alert-danger")
+            return super().get_success_url()
