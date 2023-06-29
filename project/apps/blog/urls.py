4e8e4a0d5d3f94ca1334ef7daf7224560754716c
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-06-29 16:17:34 -0300

    CRUD reseñas en blog

--- project/apps/blog/urls.py
+++ project/apps/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index
-
+from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path("", TemplateView.as_view(template_name="blog/index.html"), name="index"),
+    path("reseña/detail/<int:pk>", views.ReseñaDetail.as_view(), name="reseña_detail"),
+    path("reseña/list/", views.ReseñaList.as_view(), name="reseña_list"),
+    path("reseña/create/", views.ReseñaCreateView.as_view(), name="reseña_create"),
+    path("reseña/delete/<int:pk>", views.ReseñaDelete.as_view(), name="reseña_delete"),
+    path("reseña/update/<int:pk>", views.ReseñaUpdate.as_view(), name="reseña_update"),
 ]
--- project/apps/blog/views.py
+++ project/apps/blog/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import DetailView, ListView
+from django.urls import reverse_lazy
+from .forms import ReseñaForm
+from .models import Reseña
 
 # Create your views here.
-def index(request):
-    return render(request, 'blog/index.html')
+class ReseñaDetail(LoginRequiredMixin, DetailView):
+    model = Reseña
+
+class ReseñaList(LoginRequiredMixin, ListView):
+    model = Reseña
+
+
+class ReseñaCreateView(LoginRequiredMixin, CreateView):
+    model = Reseña
+    form_class = ReseñaForm
+    success_url = reverse_lazy("blog:reseña_list")
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+    
+class ReseñaUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Reseña
+    success_url = reverse_lazy("blog:reseña_list")
+    form_class = ReseñaForm
+
+    
+class ReseñaDelete(LoginRequiredMixin, DeleteView):
+    model = Reseña
+    success_url = reverse_lazy("blog:reseña_list")
+
